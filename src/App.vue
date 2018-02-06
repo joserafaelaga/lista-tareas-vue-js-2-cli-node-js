94d4ce5da5be1545e8badc6b5aa94d6876bdc187
José Rafael Aguilera <joserafaelaguilera@gmail.com>
2018-02-06 12:36:29 +0100

    Crear Componente Nueva Tarea

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,37 @@
   <div id="app" class="container">
     <div class="jumbotron">
       <titulo :titulo="titulo"></titulo>
+      <nueva-tarea :tareas="tareas"></nueva-tarea>
     </div>
   </div>
 </template>
 
 <script>
 import Titulo from './TituloComponent.vue'
+import NuevaTarea from './NuevaTareaComponent.vue'
+
 export default {
   components:{
-    Titulo
+    Titulo,
+    NuevaTarea
   },
   data(){
     return {
-     titulo: 'Lista de tareas'
+     titulo: 'Lista de tareas',
+     tareas: [
+      {
+        texto: 'Aprender Vue.js',
+        terminada: false
+      },
+      {
+        texto: 'Aprender Angular 2',
+        terminada: false
+      },
+      {
+        texto: 'Aprender Ionic 2',
+        terminada: false
+      },
+    ],
     }
   }
 }
---
+++ src/NuevaTareaComponent.vue
@@ -0,0 +1,31 @@
+<template>
+	<div class="input-group">	
+		<input type="text" placeholder="Escribe una nueva tarea" v-model="nuevaTarea" class="form-control" v-on:keyup.enter="agregarTarea()">
+		<span class="input-group-btn">
+			<button type="button" class="btn btn-primary" v-on:click="agregarTarea()">Agregar</button>
+		</span>			
+	</div>
+</template>
+
+<script> 
+export default {
+	data(){
+		return {
+			nuevaTarea: ''
+		}
+	},
+	props:['tareas'],
+	methods:{
+		agregarTarea(){
+			var texto = this.nuevaTarea.trim();
+			if(texto){
+				this.tareas.push({
+					texto: texto,
+					terminada: false
+				});
+			}
+			this.nuevaTarea= '';
+		}
+	}
+}	
+</script>
